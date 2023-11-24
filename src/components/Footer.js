0fecc532c183b87a8108d6ada6e8ce5ca26f52e8
Ted Smiles <tedsmiles@Teds-MacBook-Pro.local>
2023-11-24 10:52:22 +0000

    24/11/2023

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,26 @@ import './Footer.css'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faXTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { Button } from './Button';
 
 function Footer() {
   return (
     <div className='footer-container'>
-        {/*<section className="footer-subscription">
+        <section className="footer-subscription">
             <p className="footer-subscription-heading">
-                Join the newsletter to recieve .
+                Join the newsletter to recieve update and new blogs.
             </p>
             <p className="footer-subscription-text">
                 You can unsunscribe at any time.
             </p>
             <div className="input-areas">
                 <form action="">
-                    <input type="email" name='email' placeholder='Your Email'className='footer-input'/>
+                    <input type="email" name='email' placeholder='Your Email' className='footer-input'/>
                     <Button buttonStyle='btn--outline'>Subscribe</Button> 
                 </form>
             </div>
         </section>
-        <div className="footer-links">
+        {/* <div className="footer-links">
             <div className="footer-link-wrapper">
                 <div className="footer-link-items">
                     <h2>About Me</h2>
@@ -29,7 +30,7 @@ function Footer() {
                     <Link to='/'>Testimonials</Link>
                 </div>
             </div>
-        </div>*/}
+  </div> */}
 
         <section className="social-media">
             <div className="social-media-wrap">
--- src/components/SinglePage.js
+++ src/components/SinglePage.js
@@ -2,13 +2,67 @@ import React, { useEffect, useState } from 'react'
 import CardItem from './CardItem'
 import './BlogSection.css';
 import { useParams } from 'react-router-dom';
+import request from 'graphql-request';
 
-export default function SinglePage( {posts} ) {
+export default function SinglePage() {
+
+    const [posts, setPosts] = useState(null)
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const { posts } = await request(
+                'https://api-eu-west-2.hygraph.com/v2/clox175hi80uw01uqdjf6980x/master',
+                `
+            {
+                posts {
+                    id,
+                    title,
+                    datePublished,
+                    slug,
+                    coverPhoto {
+                        url
+                    },
+                    summary,
+                    content {
+                        html
+                    }
+                }
+            }
+        `
+            )
+            const post = posts.reverse()
+            setPosts(post)
+        }
+        fetchPosts()  
+    }, [])
+
     const { slug } = useParams();
 
-    console.log(posts)
-
-    const post = posts.find((post) => post.slug === slug);
-
-    return <div>Now showing post {slug}</div>
+    if (posts === null) {
+        return (
+            <h1>Error</h1>
+        )
+    }else {
+        for (let i = 0; i < posts.length; i++) {
+            
+            if (posts[i].slug === slug){
+                return (
+                    <div className='blog'>
+                    <h1>{posts[i].title}</h1>
+                    <div className="blog__container">
+                        <div className="blog__wrapper">
+                            <img className="cards__item__img" src={posts[i].coverPhoto.url} />
+                            <br />
+                            <h3>{posts[i].datePublished}</h3>
+                            
+                        </div>
+                    </div>
+                </div>
+                )
+            } else {
+                
+            }
+        } 
+        
+    }
 }
